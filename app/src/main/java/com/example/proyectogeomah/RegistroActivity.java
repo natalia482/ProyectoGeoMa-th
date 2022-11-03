99e9382d673ddedbd16ad6cc0348fbe7d3bc25f7
nataliavillalobos482 <53359650+natalia482@users.noreply.github.com>
2022-11-03 15:33:39 -0500

    vista de seleccion Matemáticas- Geografía

--- app/src/main/java/com/example/proyectogeomah/RegistroActivity.java
+++ app/src/main/java/com/example/proyectogeomah/RegistroActivity.java
@@ -1,7 +1,10 @@
 package com.example.proyectogeomah;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+    //Metodo para el botorn de la pag registro
+    public void BtnRegistro(View view){
+        Intent Registro = new Intent(this,SeleccionActivity.class);
+        startActivity(Registro);
     }
 }
